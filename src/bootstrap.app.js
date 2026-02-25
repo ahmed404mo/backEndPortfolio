3104a85aee0512923a4ae3f5f8f65720b40e992c
ahmed mokhtar <mo879938@gmail.com>
2026-02-25 13:15:02 +0200

    test 2

--- src/bootstrap.app.js
+++ src/bootstrap.app.js
@@ -48,7 +48,7 @@
 import express from 'express';
 import { globalErrorHandling } from './common/utils/response/error.response.js';
 import { authenticateDB } from './DB/connection.db.js';
-import { PORT } from '../config/config.service.js';
+import { DB_URI, PORT } from '../config/config.service.js';
 import { authRouter } from './modules/auth/index.js';
 import { projectRouter } from './modules/project/index.js';
 import { messageRouter } from './modules/message/index.js';
