9b3b593c622fb37fd99d6507a5045125ec3a2254
ahmed mokhtar <mo879938@gmail.com>
2026-04-05 01:04:13 +0200

    done

--- src/DB/connection.db.js
+++ src/DB/connection.db.js
@@ -4,24 +4,20 @@ import { DB_URI } from "../../config/config.service.js";
 let isConnected = false; 
 
 export const authenticateDB = async () => {
-  if (isConnected) {
-    console.log("🐧 DB is already connected (Using cached connection)");
-    return;
-  }
+  if (isConnected) return;
 
   try {
-    const db = await mongoose.connect(DB_URI, {
+    mongoose.set('bufferCommands', false); 
+
+    await mongoose.connect(DB_URI, {
       serverSelectionTimeoutMS: 5000, 
-      socketTimeoutMS: 45000,
-      maxPoolSize: 10, 
+      connectTimeoutMS: 10000,
     });
-
-    isConnected = db.connections[0].readyState === 1;
-    console.log(`🐧 DB connected successfully to: ${db.connection.name}`);
-
+    
+    isConnected = true;
+    console.log("✅ MongoDB Connected");
   } catch (error) {
-    console.log(`❌ fail to connected on DB ${error}`);
-console.error("Error Message:", error.message);
+    console.error("❌ Connection Fail:", error.message);
     throw error;
   }
-}
+};
