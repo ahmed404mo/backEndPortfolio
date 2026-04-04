7b11d7a310b5fc37f0c6ea40cde7267f978746f6
ahmed mokhtar <mo879938@gmail.com>
2026-04-04 20:50:20 +0200

    done

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,16 +1,25 @@
 import { Router } from "express";
 import { login, setup } from "./auth.service.js";
-import {  successResponse } from "../../common/utils/response/success.response.js";
+import { successResponse } from "../../common/utils/response/success.response.js";
 
 const router = Router();
 
 router.post("/setup", async (req, res, next) => {
-  const account = await setup();
-  return successResponse({res,status:201,data:{account}});
-});
-router.post("/login", async (req, res, next) => {
-  const account = await login(req.body);
-  return successResponse({res,data:{account}})
+  try {
+    const account = await setup();
+    return successResponse({ res, status: 201, data: { account } });
+  } catch (error) {
+    next(error); 
+  }
 });
 
-export default router
+router.post("/login", async (req, res, next) => {
+  try {
+    const account = await login(req.body);
+    return successResponse({ res, data: { account } });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
